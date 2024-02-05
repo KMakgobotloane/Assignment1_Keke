757d7c988d5f7c2c94f9753523a27d4dc3cf8e2b
KMakgobotloane <157940786+KMakgobotloane@users.noreply.github.com>
2024-02-05 16:53:44 +0200

    Add files via upload

--- css4p01.py
+++ css4p01.py
@@ -285,12 +285,261 @@ from 44 to 296   -> 575.0% change
 """
 #ANSWER Question 9: 575.0% change
 
-#Question 12
 
+#Question 12 and 13
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
 
+df = pd.read_csv('movie_dataset.csv')
 
-#Question 13
+pd.set_option('display.max_rows',None)
+
+print(df)
+
+x = df["Revenue (Millions)"].mean()
+
+df["Revenue (Millions)"].fillna(x, inplace = True)
+
+x2 = df["Metascore"].mean()
+
+df["Metascore"].fillna(x2, inplace = True)
+
+plt.figure(figsize=(2, 7))
+sns.countplot(data=df, y='Genre', order=df['Genre'].value_counts().index, palette='viridis')
+plt.xlabel('Count', fontsize=2, fontweight='bold')
+plt.ylabel('Genre', fontsize=2, fontweight='bold')
+
+plt.figure(figsize=(2, 7))
+counts = df['Genre'].value_counts()
+sns.barplot(x=counts.index, y=counts, palette='viridis')
+plt.xlabel('Genre', fontsize=2, fontweight='bold')
+plt.ylabel('Count', fontsize=2, fontweight='bold')
+plt.title('Distribution of Genres', fontsize=2, fontweight='bold')
+plt.xticks(rotation=90, fontsize=2, fontweight='bold')
+plt.show()
+
+"""
+Genre	count
+Action,Adventure,Sci-Fi	50
+Drama	48
+Comedy,Drama,Romance	35
+Comedy	32
+Drama,Romance	31
+Animation,Adventure,Comedy	27
+Action,Adventure,Fantasy	27
+Comedy,Drama	27
+Comedy,Romance	26
+Crime,Drama,Thriller	24
+Crime,Drama,Mystery	23
+Action,Adventure,Drama	18
+Action,Crime,Drama	17
+Horror,Thriller	16
+Drama,Thriller	15
+Adventure,Family,Fantasy	14
+Biography,Drama,History	14
+Action,Adventure,Comedy	14
+Biography,Drama	14
+Action,Comedy,Crime	12
+Action,Crime,Thriller	12
+Action,Adventure,Thriller	11
+Horror	11
+Crime,Drama	10
+Biography,Crime,Drama	9
+Thriller	9
+Horror,Mystery,Thriller	9
+Action,Thriller	9
+Animation,Action,Adventure	9
+Biography,Drama,Sport	8
+Adventure,Comedy,Drama	8
+Drama,Mystery,Thriller	8
+Action,Biography,Drama	8
+Mystery,Thriller	7
+Biography,Comedy,Drama	7
+Comedy,Crime,Drama	7
+Action,Drama,Thriller	7
+Action,Sci-Fi,Thriller	7
+Drama,Mystery,Romance	6
+Action,Adventure,Crime	6
+Horror,Mystery	6
+Action,Comedy	6
+Drama,Horror,Thriller	6
+Drama,Fantasy,Horror	6
+Drama,Mystery,Sci-Fi	6
+Drama,Horror,Sci-Fi	6
+Adventure,Drama,Fantasy	6
+Drama,Horror	5
+Comedy,Crime	5
+Drama,Fantasy,Romance	5
+Biography,Drama,Romance	5
+Action,Horror,Sci-Fi	5
+Action,Adventure,Mystery	5
+Drama,Sport	5
+Comedy,Drama,Music	5
+Comedy,Family	5
+Animation,Comedy,Family	5
+Drama,Romance,Sci-Fi	4
+Action,Drama,Sci-Fi	4
+Adventure,Biography,Drama	4
+Action,Adventure,Family	4
+Drama,Sci-Fi,Thriller	4
+Action,Drama,History	4
+Crime,Horror,Thriller	4
+Adventure,Drama,Family	4
+Drama,Horror,Mystery	4
+Drama,History,Thriller	4
+Action,Drama,Fantasy	3
+Mystery,Sci-Fi,Thriller	3
+Action,Sci-Fi	3
+Action,Comedy,Fantasy	3
+Crime,Drama,Horror	3
+Horror,Sci-Fi,Thriller	3
+Comedy,Drama,Family	3
+Adventure,Drama,Thriller	3
+Action,Mystery,Thriller	3
+Adventure,Fantasy	3
+Biography,Drama,Thriller	3
+Drama,War	3
+Adventure,Sci-Fi,Thriller	3
+Action,Horror,Thriller	3
+Action,Mystery,Sci-Fi	3
+Drama,Romance,Thriller	3
+Action,Fantasy,Horror	3
+Drama,Music	3
+Comedy,Fantasy,Horror	3
+Action,Crime,Mystery	3
+Action,Adventure	3
+Action,Adventure,Western	2
+Drama,Sci-Fi	2
+Adventure,Drama,Sci-Fi	2
+Action,Comedy,Horror	2
+Adventure,Comedy,Family	2
+Drama,Music,Romance	2
+Comedy,Horror,Thriller	2
+Adventure,Drama,Romance	2
+Comedy,Horror	2
+Comedy,Fantasy	2
+Comedy,Drama,Fantasy	2
+Comedy,Drama,Horror	2
+Comedy,Music	2
+Action,Adventure,Biography	2
+Action,Adventure,Horror	2
+Drama,History	2
+Adventure,Mystery,Sci-Fi	2
+Action,Adventure,Romance	2
+Action	2
+Adventure,Comedy,Romance	2
+Action,Comedy,Romance	2
+Action,Drama,Mystery	2
+Crime,Mystery,Thriller	2
+Fantasy,Horror	2
+Adventure,Comedy,Sci-Fi	2
+Animation,Adventure,Family	2
+Adventure,Horror	2
+Adventure,Comedy	2
+Action,Drama,Family	2
+Sci-Fi	2
+Drama,Fantasy	2
+Crime,Drama,History	2
+Action,Drama,Romance	2
+Action,Drama,Sport	2
+Drama,Horror,Musical	1
+Adventure,Drama,Western	1
+Drama,Fantasy,Mystery	1
+Fantasy,Horror,Thriller	1
+Drama,Mystery,War	1
+Drama,Thriller,War	1
+Adventure,Drama	1
+Romance,Sci-Fi,Thriller	1
+Comedy,Romance,Western	1
+Animation,Fantasy	1
+Drama,Family	1
+Adventure,Biography	1
+Comedy,Crime,Thriller	1
+Animation,Comedy,Drama	1
+Fantasy,Mystery,Thriller	1
+Adventure,Biography,Crime	1
+Action,Comedy,Sport	1
+Comedy,Drama,Musical	1
+Drama,Fantasy,Music	1
+Crime,Drama,Fantasy	1
+Comedy,Family,Romance	1
+Comedy,Western	1
+Biography,Drama,Family	1
+Drama,Fantasy,Musical	1
+Adventure,Family	1
+Comedy,Horror,Sci-Fi	1
+Crime,Thriller	1
+Action,Crime,Sci-Fi	1
+Adventure,Comedy,Fantasy	1
+Adventure	1
+Action,Thriller,War	1
+Comedy,Horror,Romance	1
+Animation,Drama,Romance	1
+Biography,Drama,Mystery	1
+Drama,Family,Music	1
+Mystery,Romance,Thriller	1
+Adventure,Fantasy,Mystery	1
+Drama,Romance,War	1
+Action,Comedy,Sci-Fi	1
+Drama,History,War	1
+Action,Drama,War	1
+Comedy,Drama,Thriller	1
+Mystery,Romance,Sci-Fi	1
+Action,Drama,Horror	1
+Action,Fantasy	1
+Drama,Musical,Romance	1
+Drama,Fantasy,War	1
+Drama,Family,Fantasy	1
+Thriller,War	1
+Action,Comedy,Mystery	1
+Drama,Western	1
+Mystery,Thriller,Western	1
+Comedy,Family,Musical	1
+Adventure,Crime,Mystery	1
+Action,Fantasy,War	1
+Romance,Sci-Fi	1
+Horror,Mystery,Sci-Fi	1
+Animation,Drama,Fantasy	1
+Biography,Comedy,Crime	1
+Adventure,Drama,War	1
+Adventure,Drama,History	1
+Action,Comedy,Drama	1
+Comedy,Music,Romance	1
+Action,Horror	1
+Crime,Drama,Music	1
+Action,Horror,Mystery	1
+Drama,Fantasy,Thriller	1
+Animation,Action,Comedy	1
+Comedy,Romance,Sport	1
+Action,Fantasy,Thriller	1
+Biography,History,Thriller	1
+Adventure,Drama,Horror	1
+Sci-Fi,Thriller	1
+Action,Crime,Sport	1
+Adventure,Horror,Mystery	1
+Comedy,Fantasy,Romance	1
+Animation,Family,Fantasy	1
+Action,Horror,Romance	1
+Action,Biography,Crime	1
+Comedy,Sci-Fi	1
+Action,Comedy,Family	1
+Action,Crime,Fantasy	1
+Comedy,Mystery	1
+Adventure,Comedy,Horror	1
+Comedy,Family,Fantasy	1
+"""
+# ANSWER Q12: The series has a size of 207, as seen on the Variable Explorer.
 
 
 # 1.
